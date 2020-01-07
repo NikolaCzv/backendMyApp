239f2b79fc46a41c2dec06d1dad1c3c23a69e739
NikolaCzv <nikola.raicic@hotmail.com>
2020-01-07 13:40:05 -0600

    models set

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+    has_many :posts
+    has_many :liked_posts, through: :likes, source: :post
+    has_many :commented_posts, through: :comments, source: :post
+
+    has_many :followed_users, foreign_key: :follower_id, class_name: 'Follow'
+    has_many :followees, through: :followed_users
+
+    has_many :following_users, foreign_key: :followee_id, class_name: 'Follow'
+    has_many :followers, through: :following_users
+
+end
