e85edc5a7b1bf2bb3736a20a7fcd12ee3793fadb
NikolaCzv <nikola.raicic@hotmail.com>
2020-01-08 14:29:17 -0600

    login done

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,8 +7,7 @@ class Api::V1::UsersController < ApplicationController
     def create
         user = User.find_by(username: params['username'])
         if user && user.authenticate(params['password'])
-          token =  JWT.encode({user_id: user.id}, 'secretkey', 'HS256')
-    
+          token =  JWT.encode({id: user.id}, 'secretkey', 'HS256')
           render json: { id: user.id, username: user.username, token: token }
         else
           render json: { error: 'invalid credentials' }, status: 401
@@ -18,7 +17,7 @@ class Api::V1::UsersController < ApplicationController
     def show
         token = request.headers['Authorization'].split(' ')[1]
         decode = JWT.decode token, 'secretkey', true, { algorithm: 'HS256' }
-        user_id = decode[0]['user_id']
+        user_id = decode[0]['id']
         user = User.find(user_id)
     
         if(user)
--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:create, :index]
+      resources :users, only: [:create, :index, :show]
       post '/login', to: 'users#create'
-      get '/profile', to: 'users#profile'
+      get '/current_user', to: 'users#show'
     end
   end
 end
