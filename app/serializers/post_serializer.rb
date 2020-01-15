6c92f717f59cb41d59add561e981d5c674af3d0d
NikolaCzv <nikola.raicic@hotmail.com>
2020-01-15 17:23:37 -0600

    first video on adding photos

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
-    attributes :id, :text, :pic_url, :user_id, :likes, :comments, :likes
+    attributes :id, :text, :pic_url, :user_id, :likes, :comments
 
     def likes
         object.likes.count
