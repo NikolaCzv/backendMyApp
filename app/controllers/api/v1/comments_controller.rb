63637e005451ce496fe75d7d0b632e3de836be65
NikolaCzv <nikola.raicic@hotmail.com>
2020-01-21 16:29:29 -0600

    add comments

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,19 @@
 class Api::V1::CommentsController < ApplicationController
+    def index
+        comments = Comment.all
+
+        render json: comments
+    end
+
+    def show
+        comment = Comment.find(params[:id])
+
+        render json: comment
+    end
+
+    def create
+        comment = Comment.create(user_id: params[:user_id], post_id: params[:post_id], content: params[:content])
+        
+        render json: comment
+    end
 end
