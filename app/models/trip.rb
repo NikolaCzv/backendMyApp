f43925bd0f3b6185083bf70d731e4df8a2786e8e
NikolaCzv <nikola.raicic@hotmail.com>
2020-01-23 11:00:19 -0600

    hope last commit

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,8 @@
 class Trip < ApplicationRecord
+    validates :start_date, presence: true
+    validates :end_date, presence: true
+    
     belongs_to :user
     belongs_to :renter, class_name: 'User', optional: true
+
 end
