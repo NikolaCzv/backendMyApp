97caaf91e6a9c2dde5a24f3b16b86adc739c994b
github NikolaCzv <nikola.raicic@hotmail.com>
2020-05-20 18:16:30 -0500

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # test
+  # tes
   namespace :api do
     namespace :v1 do
       resources :trips, only: [:create, :index, :show, :update, :destroy]
