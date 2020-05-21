284348999578d87231a88c76ba27b8fccb97a90d
github NikolaCzv <nikola.raicic@hotmail.com>
2020-05-20 19:21:04 -0500

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #test
   root 'welcome#index'
   namespace :api do
     namespace :v1 do
